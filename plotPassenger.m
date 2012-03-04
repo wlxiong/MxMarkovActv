19829065af327732d86abf3bbcaf841b66c0d5bc
Yiliang Xiong <wlxiong@gmail.com>
2012-03-05 04:52:44 +0800

    some minor changes

--- plotPassenger.m
+++ plotPassenger.m
@@ -5,13 +5,13 @@ hold on
 box on
 flows = 1000-sum(png,2);
 x1 = genTimes(13, 12+size(png,1));
-h1 = bar(x1, [png(:,2) flows png(:,1)], 'stacked');
+h1 = bar(x1, [png(:,1) flows png(:,2)], 'stacked');
 datetick('x','HHPM')
 colormap summer
 set(get(h1(1),'BaseLine'),'LineWidth',1,'LineStyle',':')
 x2 = genTimes(12, 12+size(png,1)+1);
 xlim([min(x2), max(x2)])
 ylim([0 max(png(:))])
-legend('Work', 'Travel', 'Home', 'Location', 'NorthWest')
-ylabel('Number of Travelers')
+legend('Home', 'Travel', 'Work', 'Location', 'NorthWest')
+ylabel('Population')
 xlabel('Time of the day')
